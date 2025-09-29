908a13a32f3e72794855774458c8f8b8db2c4b9e
colamanlabs developer <46599184+colamanlabs@users.noreply.github.com>
2025-09-29 21:01:19 +0900

    Add files via upload
    1. slf4j, logback 설정까지 진행함

---
+++ test_20250929/src/main/java/com/colamanlabs/netty/App.java
@@ -0,0 +1,17 @@
+package com.colamanlabs.netty;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ *
+ */
+@Slf4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        log.debug(String.format("[App/main]"));
+    }
+}
